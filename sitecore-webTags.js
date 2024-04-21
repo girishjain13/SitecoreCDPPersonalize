8f8db664d839196d3693a481b0820599df77981c
girishjain13 <123942958+girishjain13@users.noreply.github.com>
2024-04-21 10:46:06 +0530

    Add files via upload

--- sitecore-webTags.js
+++ sitecore-webTags.js
@@ -6,10 +6,7 @@
       client_key: 'sndbxus06p9cxhoqoiowkr1sbq5casz3', // Replace with your client key
       target: 'https://api-us.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
       cookie_domain: '.sitecore-cdp-personalize.vercel.app', // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
-      javascriptLibraryVersion: '1.4.9', // Replace with the latest Boxever JavaScript library version"
-      pointOfSale: 'MobileRetail', // Replace with the same point of sale configured in system settings"
-      web_flow_target: 'https://d35vb5cccm4xzp.cloudfront.net', // Replace with path for the Amazon CloudFront CDN for Sitecore Personalize"
-      // Customize the async and defer script loading attributes
+      
     };
     // Import the Boxever library asynchronously
     (function () {
